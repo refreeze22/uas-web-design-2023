ae58984ce47422474e2c7151b0bececa43b4c13d
refreeze22 <92356831+refreeze22@users.noreply.github.com>
2023-07-03 10:00:52 +0700

    init c

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+function popNotification() {
+	Swal.fire({
+		icon: 'info',
+		title: 'Information',
+		text: 'Every business has an origin story worth telling, and usually, one that justifies why you even do business and have clients.',
+		showConfirmButton: true,
+		confirmButtonText: 'Understand'
+	})
+}
